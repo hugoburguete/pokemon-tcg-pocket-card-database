4bcf22a2b4208e3f907558afbd3a76282253f710
Hugo Burguete <hugoburguete@hotmail.com>
2025-12-28 20:25:41 +0000

    feat: add missing sets

--- cards/en/index.js
+++ cards/en/index.js
@@ -1,19 +1,33 @@
 const geneticApex = require('./a1-genetic-apex.json');
-const promo = require('./promo.json');
+const promoA = require('./promo-a.json');
+const deluxePackEx = require('./a4b-deluxe-pack-ex.json');
 const mythicalIsland = require('./a1a-mythical-island.json');
 const spaceTimeSmackdown = require('./a2-space-time-smackdown.json');
 const triumphantLight = require('./a2a-triumphant-light.json');
 const shiningRevelry = require('./a2b-shining-revelry.json');
 const celestialGuardians = require('./a3-celestial-guardians.json');
 const extradimensionalCrisis = require('./a3a-extradimensional-crisis.json');
+const eeveeGrove = require('./a3b-eevee-grove.json');
+const wisdomOfSeaAndSky = require('./a4-wisdom-of-sea-and-sky.json');
+const secludedSprings = require('./a4a-secluded-springs.json');
+const promoB = require('./promo-b.json');
+const megaRising = require('./b1-mega-rising.json');
+const crimsonBlaze = require('./b1a-crimson-blaze.json');
 
 module.exports = {
   geneticApex,
-  promo,
+  promoA,
   spaceTimeSmackdown,
   triumphantLight,
   mythicalIsland,
   shiningRevelry,
   celestialGuardians,
   extradimensionalCrisis,
+  eeveeGrove,
+  wisdomOfSeaAndSky,
+  secludedSprings,
+  deluxePackEx,
+  promoB,
+  megaRising,
+  crimsonBlaze,
 };
